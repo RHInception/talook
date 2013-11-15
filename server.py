dd0593d7b7a2d8df57c364ee636cdaf57dec7f0f
Steve Milner <stevem@gnulinux.net>
2013-11-15 10:55:58 -0500

    Moving oldserver into server so there is no need for a package.

--- server.py
+++ server.py
@@ -277,6 +277,83 @@ def run_server(host, port):
     httpd.serve_forever()
 
 
+def run_old_server(host, port):
+    """
+    Code for running the old server.
+    """
+
+    import urllib
+    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+    def create_wsgi_wrapper(wsgi_app):
+        """
+        Wraps a WSGI application for use.
+        """
+
+        class WSGIWrapperHandler(BaseHTTPRequestHandler):
+
+            def start_response(self, status, headers):
+                """
+                A fake WSGI start_response method.
+                """
+                # Handle status info
+                # TODO: Handle network errors better.
+                status_data = status.split(' ')
+                if len(status_data) > 1:
+                    self.send_response(int(status_data[0]), status_data[1])
+                else:
+                    self.send_response(int(status_data[0]))
+                # Iterate over headers and send them out
+                for name, value in headers:
+                    self.send_header(name, value)
+                self.end_headers()
+
+            def handle(self):
+                """
+                Overrides handle so that the environ is set.
+                """
+                self.environ = self.server._environ.copy()
+                BaseHTTPRequestHandler.handle(self)
+
+            def do_GET(self):
+                """
+                Since we only do GET we only need to define do_GET.
+                """
+                if '?' in self.path:
+                    path, query = self.path.split('?', 1)
+                else:
+                    path, query = (self.path, '')
+
+                self.environ['QUERY_STRING'] = query
+                self.environ['PATH_INFO'] = urllib.unquote(path)
+
+                for chunk in wsgi_app(self.environ, self.start_response):
+                    self.wfile.write(chunk)
+
+        return WSGIWrapperHandler
+
+    class WSGILiteServer(HTTPServer):
+        """
+        Not 100% WSGI compliant but enough for what we need.
+        """
+
+        def __init__(self, *args, **kwargs):
+            """
+            Creates an instance of a fake WSGI server.
+            """
+            HTTPServer.__init__(self, *args, **kwargs)
+            self._environ = {
+                'SERVER_NAME': self.server_name,
+                'GATEWAY_INTERFACE': 'CGI/1.1',
+                'SERVER_PORT': str(self.server_port),
+                'REMOTE_HOST': '',
+            }
+
+    server = WSGILiteServer((host, port), create_wsgi_wrapper(make_app()))
+    print "server listening on http://%s:%s" % (host, port)
+    server.serve_forever()
+
+
 if __name__ == "__main__":
     import platform
     # Using optparse since argparse is not available in 2.5
@@ -295,7 +372,6 @@ if __name__ == "__main__":
     try:
         # Fall back to old school container if on 2.4.x
         if py_version >= '2.4.0' and py_version < '2.5.0':
-            from oldserver import run_old_server
             run_old_server(options.listen, options.port)
         # Else use the builtin wsgi container
         elif py_version >= '2.5.0':
