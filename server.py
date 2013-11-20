18f4581459c291f731760d0423c5cf1af82e7145
Steve Milner <stevem@gnulinux.net>
2013-11-20 17:04:55 -0500

    Added threading so that the standalone servers can autoreload if -r is used. Closes #7.

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Self contained stats consumer.
 import datetime
 import os
 import re
+import threading
+import time
 
 try:
     import json
@@ -24,18 +26,20 @@ def create_logger(name, filename,
     """
     Creates a logger instance.
     """
-    logfile = os.path.sep.join([os.path.realpath(
-        json.load(open(os.environ['TALOOK_CONFIG_FILE'], 'r'))['logdir']),
-        filename])
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    if not os.path.exists(os.path.dirname(logfile)):
-        os.makedirs(os.path.dirname(logfile))
-    handler = logging.handlers.TimedRotatingFileHandler(
-        logfile, 'd')
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter(format))
-    logger.addHandler(handler)
+    if len(logger.handlers) == 0:
+        logger.setLevel(logging.INFO)
+        logfile = os.path.sep.join([os.path.realpath(
+            json.load(open(os.environ['TALOOK_CONFIG_FILE'], 'r'))['logdir']),
+            filename])
+
+        if not os.path.exists(os.path.dirname(logfile)):
+            os.makedirs(os.path.dirname(logfile))
+        handler = logging.handlers.TimedRotatingFileHandler(
+            logfile, 'd')
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(logging.Formatter(format))
+        logger.addHandler(handler)
     return logger
 
 
@@ -51,8 +55,17 @@ class Router(object):
         rules is a dictionary defining uri: WSGIApplication
         """
         self._rules = {}
+        self.logger = create_logger('talook', 'talook_app.log')
         for uri, app in rules.items():
             self._rules[uri] = {'app': app, 'regex': re.compile(uri)}
+
+    def reload(self):
+        """
+        Reruns init for all mounted WSGI apps.
+        """
+        self.logger.info('Reloading config')
+        for key, value in self._rules.items():
+            value['app'].__init__()
 
     def __call__(self, environ, start_response):
         """
@@ -240,7 +253,7 @@ class QueryHostHandler(BaseHandler):
         return self.return_404(start_response)
 
 
-def run_server(host, port):
+def create_server(host, port):
     """
     If the server is called directly then serve via wsgiref.
     """
@@ -257,16 +270,14 @@ def run_server(host, port):
                 self.log_date_time_string(),
                 format % args))
 
-    application = make_app()
+    app = make_app()
 
-    httpd = make_server(
-        host, int(port), application,
-        handler_class=TalookHandler)
-    print "server listening on http://%s:%s" % (host, port)
-    httpd.serve_forever()
+    return (make_server(
+        host, int(port), app,
+        handler_class=TalookHandler), app)
 
 
-def run_old_server(host, port):
+def create_old_server(host, port):
     """
     Code for running the old server.
     """
@@ -348,9 +359,76 @@ def run_old_server(host, port):
                 'REMOTE_HOST': '',
             }
 
-    server = WSGILiteServer((host, port), create_wsgi_wrapper(make_app()))
-    print "server listening on http://%s:%s" % (host, port)
-    server.serve_forever()
+    app = make_app()
+    return (WSGILiteServer((host, port), create_wsgi_wrapper(app)), app)
+
+
+class ServerThread(threading.Thread):
+    """
+    Thread for the server to run in.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Creates the thread object and a logger.
+        """
+        threading.Thread.__init__(self, *args, **kwargs)
+        self.logger = create_logger('talook', 'talook_app.log')
+
+    def terminate(self):
+        """
+        How to signal the thread to end.
+        """
+        self.logger.debug(
+            'ServerThread %s has been asked to terminate.' % self.getName())
+        self.server.shutdown()
+
+    def start(self, server):
+        """
+        How to start the thread.
+        """
+        self.server = server
+        threading.Thread.start(self)
+
+    def run(self):
+        """
+        How to run.
+        """
+        self.server.serve_forever()
+        self.logger.info('ServerThread %s is exiting NOW!' % self.getName())
+        raise SystemExit(0)
+
+
+class ConfigPoller(ServerThread):
+    """
+    Thread for the config file poller to run in.
+    """
+
+    #: Variable to note if the poller should terminate
+    _terminate = False
+
+    def start(self, app):
+        self.app = app
+        threading.Thread.start(self)
+
+    def terminate(self):
+        """
+        How to signal the thread to end.
+        """
+        self._terminate = True
+
+    def run(self):
+        """
+        How to run.
+        """
+        mtime = os.stat(os.environ['TALOOK_CONFIG_FILE']).st_mtime
+        while not self._terminate:
+            current_mtime = os.stat(os.environ['TALOOK_CONFIG_FILE']).st_mtime
+            if mtime != current_mtime:
+                mtime = current_mtime
+                self.app.reload()
+            time.sleep(1)
+        raise SystemExit(0)
 
 
 def make_app():
@@ -379,21 +457,46 @@ if __name__ == "__main__":
     parser.add_option(
         '-l', '--listen', dest='listen', default='0.0.0.0',
         help='Address to listen on. (Default: 0.0.0.0)')
+    parser.add_option(
+        '-r', '--reload', dest='reload', default=False, action='store_true',
+        help='Enable reloading on config change. (Default: False)')
 
     (options, args) = parser.parse_args()
 
     os.environ['TALOOK_CONFIG_FILE'] = options.config
     py_version = platform.python_version()
-    try:
-        # Fall back to old school container if on 2.4.x
-        if py_version >= '2.4.0' and py_version < '2.5.0':
-            run_old_server(options.listen, options.port)
-        # Else use the builtin wsgi container
-        elif py_version >= '2.5.0':
-            run_server(options.listen, options.port)
-        else:
-            print 'Untested Python version in use: %s' % py_version
-            raise SystemExit(1)
-    except KeyboardInterrupt:
-        print "shutting down..."
-        raise SystemExit(0)
+
+    server = None
+    # Fall back to old school container if on 2.4.x
+    if py_version >= '2.4.0' and py_version < '2.5.0':
+        server, app = create_old_server(options.listen, options.port)
+    # Else use the builtin wsgi container
+    elif py_version >= '2.5.0':
+        server, app = create_server(options.listen, options.port)
+    else:
+        print 'Untested Python version in use: %s' % py_version
+        raise SystemExit(1)
+
+    if options.reload:
+        config_poller_thread = ConfigPoller()
+        config_poller_thread.setDaemon(True)
+        config_poller_thread.start(app)
+
+    print "server listening on http://%s:%s" % (options.listen, options.port)
+    server_thread = ServerThread()
+    server_thread.setDaemon(True)
+    server_thread.start(server)
+
+    # Main loop (even though all real work is in threads)
+    while True:
+        try:
+            time.sleep(10)
+        except KeyboardInterrupt:
+            if options.reload:
+                config_poller_thread.terminate()
+                config_poller_thread.join()
+            server_thread.terminate()
+            server_thread.join()
+            raise SystemExit(0)
+
+    raise SystemExit(0)
